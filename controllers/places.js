98e5de768c6d4581e5820c4e3f87977655ddd3e9
andrewdiaz20 <andrewdiazn20@gmail.com>
2023-12-03 10:35:44 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,27 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
+  
+  
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
